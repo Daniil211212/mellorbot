f39d914c996f6c815d56cea007cd09887e2ff081
Daniil211212 <62184970+Daniil211212@users.noreply.github.com>
2021-07-31 17:40:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 main_token = '67f3d7f2fecbcfb643397182811d26bf3942e01021a406f365f5126f5e32ff68e1c90da290e4ee9ed2756'
 
 vk_session = vk_api.VkApi(token=main_token)
-longpoll = VkBotLongPoll(vk_session, 206197090)
+longpoll = VkBotLongPoll(vk_session)
 
 def sender(id, text):
     vk_session.method('messages.send', {'chat_id': id, 'message' : text, 'random_id' : 0})
